17dff3ed9cc0ed2ed6df94107ba4ec83151470ea
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2025-01-07 13:29:12 -0300

    fix: app.js

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const routes = require('./routes/route.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// Configuração do CORS
 app.use(cors({
-    origin: 'http://127.0.0.1:5500', // Ajuste para a URL do seu frontend
+    origin: ['http://127.0.0.1:5500', 'https://trabalhomunchikingame.vercel.app/'], // Adicione a URL do front-end em produção
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
 
-
+// Configuração do Body Parser
 app.use(bodyParser.json());
+
+// Rotas da API
 app.use('/api', routes);
 
 const port = process.env.PORT || 3000;
 
+// Inicialização do servidor
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
